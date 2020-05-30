cf51377392ba75259173617191120e482baf2f04
Swapan <swap.math@gmail.com>
2020-05-31 01:35:42 +0530

    Level 3 Hashing Password

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+var md5 = require("md5");
 
 
 const ejs = require("ejs");
@@ -28,7 +29,7 @@ const userSchema = new mongoose.Schema({
 
 
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields: ["password"]});
+// userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields: ["password"]});
 
 
 const User = mongoose.model("User", userSchema);
@@ -51,7 +52,7 @@ app.post("/register",function(req, res){
 
     newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err){
@@ -68,7 +69,7 @@ app.post("/register",function(req, res){
 
 app.post("/login", function(req, res){
 const username = req.body.username;
-const password = req.body.password;
+const password = md5(req.body.password);
 
 User.findOne({email: username},function(err, foundUser){
     if(err){
